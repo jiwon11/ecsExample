f57c6da7b75cd0a04e5599b2ccf060bb346dbaf6
정지원 <jeongjiwon@jeongjiwon-ui-MacBookPro.local>
2023-05-14 22:00:44 +0900

    github actions ecs workflow update

---
+++ app/src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AppService {
+  constructor(private config: ConfigService) {}
+
+  getHello(): string {
+    const author = this.config.get('AUTHOR');
+    return `Hello World! By ${author}`;
+  }
+}
